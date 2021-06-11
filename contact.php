87e5de0870f72f28f5dfea61ded5917d624c5936
nicoleabdilla20 <80816448+nicoleabdilla20@users.noreply.github.com>
2021-06-11 08:54:21 +0200

    Comments
    Today I have added comments to the code since there was only a little and I added 404.html and layout.html did not realise It did not get uploaded last time

--- contact.php
+++ contact.php
@@ -1,7 +1,9 @@
-<?php	
+<!-- Made by Nicole Abdilla @nicoleabdilla20 -->
+<?php
+    session_start();
+    require_once __DIR__.'/bootstrap.php';
     require "admin/db.php";	
 ?>
-
 <!DOCTYPE html>
     <html>
     <head>
@@ -9,22 +11,21 @@
         <meta name = "viewport" content = "width=device-width, initial scale=1">
         <meta name = "keyboards" content = "Home, Menu, Favourites, About, Contact">
         <script src="js/button.js"></script>
-
         <!--stylesheet-->
         <link rel = "stylesheet" type = "text/css" href = "css/style.css">
     </head>
     <body>
-        <div class = "topnav">
+    <div class = "topnav">
             <a href = "index.html"><img src = "images/logo.png" alt = "logo"></a>
             <a class = " page" href = "index.html">Home</a>
-            <a class = "active page" href = "menu.php">Menu</a>
-            <a class = "page" href = "details.php">Details</a>
-            <a class = "page" href = "favourites.php">Favourites</a>
             <a class = "page" href = "about.html">About</a>
-            <a class = "page" href = "contact.html">Contact</a>
+            <a class = "active page" href = "contact.php">Contact</a>
+            <a class = "page" href = "menu.php">Menu</a>
+            <a class = "page" href = "favourites.php">Favourites</a>
         </div>
         <div class = "contact">
-                <h2>Contact Forms</h2>           
+                <h2>Contact Forms</h2>       
+                <!-- below are functions retrieves fro js/button.js -->    
                 <button id="reservation" class = "bt" onclick ="reservation()">Book a Reservation<p>Press Here</p></button>
                 <button id="query" class="bt" onclick = "query()">Complaints & Queries <p>Press Here</p></button>              
         </div>
--- menu.php
+++ menu.php
@@ -1,14 +1,17 @@
-<?php	
-    //require_once __DIR__.'/admin/bootstrap.php';
+<!-- Made by Nicole Abdilla @nicoleabdilla20 -->
+<?php
+	session_start();
+    require_once __DIR__.'/bootstrap.php';
 	require 'admin/db.php';
-    $sql = "SELECT* FROM food";
-    $res =mysqli_query($db, $sql);
-    $count = mysqli_num_rows($res);
+    $sql = "SELECT* FROM food"; //food is a table from restaurant database
+    $res = mysqli_query($db, $sql);
+    $count = mysqli_num_rows($res); //returns number of rows 
         //Retrieval of menu
-        if($count>0)
+        if($count>0) //will loop until it has gone through all the rows in the table
         {
             while($row=mysqli_fetch_assoc($res))
             {
+                //the below are variables storing content of the current row no. in the table
                 $ID = $row['id'];
                 $Category = $row['category'];
                 $Dish = $row['dish'];
@@ -17,12 +20,12 @@
                 $main = "";
                 $starter = "";
                 $dessert = "";
-                $get_recent = $db->query("SELECT * FROM food");
-                if($get_recent->num_rows) {
+                $get_recent = $db->query("SELECT * FROM food"); // -> is used to obtain a method and the properties of an object
+                if($get_recent->num_rows) { //get_recent is a variable to get the current items according to $count 
                     while($row = $get_recent->fetch_assoc()) { 
-                        //breakfast                       
-                        if($row['category'] == "breakfast") {                          
-                            $bfast .= "<div class='category'>                  
+                        //BreakFast - the below is the outputting process of the data in the table                       
+                        if($row['category'] == "breakfast") {  //.= is a concatenation assignment, type of string operator                    
+                            $bfast .= "<div class='category'>              
 							<a href='detail.php?fid=".$row['id']."'>
                                 <img src='images/menu/".$row['id'].".jpg' width='80px' height='80px' /> 
                                 <div class='menu'>
@@ -31,7 +34,7 @@
                                 </div>
                                 </a>      
                             </div>";
-                        //main
+                        //Main  - the below is the outputting process of the data in the table  
                         }else if($row['category'] == "main") {
                             $main .=	"<div class='category'>
 							<a href='detail.php?fid=".$row['id']."'>
@@ -42,18 +45,18 @@
                                 </div>
                             </a>
                             </div>";
-                        //starter
+                        //Starter - the below is the outputting process of the data in the table  
                         }else if($row['category'] == "starter") {
                             $starter .= "<div class='category'>
 							<a href='detail.php?fid=".$row['id']."'>
                                     <img src='images/menu/".$row['id'].".jpg' width='80px' height='80px' /> 
                                     <div class='menu'>              
                                         <h4>".$row['dish']."</h4>
-                                         <p class='price'>€".$row['price']."</p>
+                                         <p class='price'>€".$row['price']."</p>oi
                                     </div>
                                 </a>
                             </div>";
-                        //dessert        
+                        //Dessert - the below is the outputting process of the data in the table        
                         }else if($row['category'] == "dessert") {
                             $dessert .= "<div class='category'>
 							<a href='detail.php?fid=".$row['id']."'>
@@ -67,12 +70,12 @@
                         }     
                     }
                 }else{
+                    //condition: when the if loop meets an error output 404.html
                     echo $twig->render('404.html');
                 }
             }
         }
 ?>
-
 <!DOCTYPE html>
     <html>
     <head>
@@ -86,38 +89,37 @@
         <div class = "topnav">
             <a href = "index.html"><img src = "images/logo.png" alt = "logo"></a>
             <a class = " page" href = "index.html">Home</a>
-            <a class = "active page" href = "menu.php">Menu</a>
-            <a class = "page" href = "detail.php">Details</a>
-            <a class = "page" href = "favourites.php">Favourites</a>
             <a class = "page" href = "about.html">About</a>
-            <a class = "page" href = "contact.html">Contact</a>
+            <a class = "page" href = "contact.php">Contact</a>
+            <a class = "active page" href = "menu.php">Menu</a>
+            <a class = "page" href = "favourites.php">Favourites</a>
         </div>
         <div class = "title">
                 <h2>Food Menu</h2>
                 <h3>Press on the food item for more detail</h3>
         </div>
-        <div class="sec" onclick="remove_class()">
+        <div class="sec">
             <div class="menu_content">	
-                <h1>Breakfast</h1>	
+                <h1>Breakfast</h1>	<!--I am aware that in text css is not best practice but did not how I could apply css to php in another way. -->
 			    <?php echo ($bfast == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your favourite list is empty</h3>" : $bfast; ?>		
-                <p class="rmv"></p>
+                <p class="rmv"></p> <!-- the css of this does not allow elements to float on both sides. -->
             </div>
         </div>
-        <div class="sec" onclick="remove_class()">
+        <div class="sec">
             <div class="menu_content">	
                 <h1>Starter</h1>		
                 <?php echo ($starter == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your favourite list is empty</h3>" : $starter; ?>			
                 <p class="rmv"></p>
             </div>
         </div>
-        <div class="sec" onclick="remove_class()">
+        <div class="sec">
             <div class="menu_content">
                 <h1>Main Course</h1>
                 <?php echo ($main == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your favourite list is empty</h3>" : $main; ?>
                 <p class="rmv"></p>
             </div>
         </div>
-        <div class="sec" onclick="remove_class()">
+        <div class="sec">
             <div class="menu_content">
                 <h1>Dessert</h1>
                 <?php echo ($dessert == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your favourite list is empty</h3>" : $dessert; ?>
